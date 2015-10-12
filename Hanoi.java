afdcb568dc22efda08e200908cd703daefe6d5e0
unknown <sbaird02@CSPC121.dcs.bbk.ac.uk>
2015-10-12 16:40:30 +0100

    Standard method for Hanoi Tower

--- Hanoi.java
+++ Hanoi.java
@@ -8,15 +8,20 @@ public class Hanoi {
 	private void hanoiRunner() {
 
 		int disc = 64;
-		System.out.println("Tower of Hanoi with " + disc + " discs took " + hanoiRecurse(disc) + " moves");
+		System.out.println("Tower of Hanoi with " + disc + " discs took (Standard method)" + hanoiRecurseStandardMethod(disc) + " moves");
+		System.out.println("Tower of Hanoi with " + disc + " discs took (Steve's method)" + hanoiRecurseSteveMethod(disc) + " moves");
 		System.out.println("Swapping " + disc + " odd and even discs took " + swapEvenAndOddRecurse(disc) + " moves");
 
 	}
 	
-	private double hanoiRecurse(int disc) {
+	private double hanoiRecurseStandardMethod(int disc) {
 		
-
-		return disc ==1 ? 2 : hanoiRecurse(disc-1) * 3 + 2;
+		return disc ==1 ? 1 : hanoiRecurseStandardMethod(disc-1) * 2 + 1;
+	}
+	
+	private double hanoiRecurseSteveMethod(int disc) {
+		
+		return disc ==1 ? 2 : hanoiRecurseSteveMethod(disc-1) * 3 + 2;
 	}
 	
 	private double swapEvenAndOddRecurse(int disc) {
